72cc67e24f845b0fa6db99d39d33ae7b920e4b3a
Takahide Nogayama <nogayama@jp.ibm.com>
2017-05-29 16:39:10 +0900

    first

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='hello',
+    version='0.1',
+    description='The funniest joke in the world',
+    url='http://github.com/storborg/funniest',
+    author='Flying Circus',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    install_requires=[
+        'simplejson',
+    ],
+    zip_safe=False)
